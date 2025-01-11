878d10ebe764806d31a69d7ec3c9ba6ac49fb30e
Henrique Sanches <sanches.hdigital@gmail.com>
2025-01-11 20:00:26 -0300

    Revert "feat: responsivity and react-icons"
    This reverts commit 7c47e98a520d5fa93d5b5e63af55825590d531f8.

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,16 +1,41 @@
-import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 import {
+  NavContainer,
+  NavList,
+  NavItem,
   MainContent,
   Title,
   Container,
+  NavGroup
 } from '../styles/components/Index.styles';
 
 function Index() {
   return (
     <Container>
-      <Navbar />
+      <NavContainer>
+        <NavList>
+          <NavGroup>
+            <NavItem>
+              <Link to="/">Página Inicial</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/pacientes">Pacientes</Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/agenda">Agenda</Link>
+            </NavItem>
+          </NavGroup>
+          <NavGroup>
+            <NavItem>
+              <Link to="/login">Login</Link>
+            </NavItem>
+          </NavGroup>
+        </NavList>
+      </NavContainer>
+      
       <MainContent>
-        <Title>Gerenciador Inteligente de Prontuários</Title>
+        <Title>LumenPsi - Iluminando os Caminhos da sua Mente</Title>
+        <h2>Gerenciador Inteligente de Prontuários</h2>
       </MainContent>
     </Container>
   );
